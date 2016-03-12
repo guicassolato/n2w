b3cfd9217267e00f037bce6e01e801bce5bb4659
Guilherme Cassolato <guicassolato@gmail.com>
2016-03-12 18:05:35 -0300

    Support for numbers smaller than 20

--- src/com/worldpay/numbers/n2w/NumberToWord.java
+++ src/com/worldpay/numbers/n2w/NumberToWord.java
@@ -1,5 +1,36 @@
 package com.worldpay.numbers.n2w;
 
 public class NumberToWord {
+	
+	private static final String[] SIMPLE_NAMED_NUMBERS = {
+		"zero",
+		"one",
+		"two",
+		"three",
+		"four",
+		"five",
+		"six",
+		"seven",
+		"eight",
+		"nine",
+		"ten",
+		"eleven",
+		"twelve",
+		"thirteen",
+		"fourteen",
+		"fifteen",
+		"sixteen",
+		"seventeen",
+		"eighteen",
+		"nineteen"
+	};
+	
+	private static String getSimpleNamedNumberAsWord(int n) {
+		return SIMPLE_NAMED_NUMBERS[n];
+	};
+	
+	public static String getNumberAsWords(int n) {
+		return getSimpleNamedNumberAsWord(n);
+	}
 
 }
--- src/com/worldpay/numbers/n2w/tdd/NumberToWordTest.java
+++ src/com/worldpay/numbers/n2w/tdd/NumberToWordTest.java
@@ -1,14 +1,17 @@
 package com.worldpay.numbers.n2w.tdd;
 
 import org.junit.Test;
+
+import com.worldpay.numbers.n2w.NumberToWord;
+
 import static org.junit.Assert.*;
 
 public class NumberToWordTest {
 	
 	@Test
 	public void shouldConvertSmallNumberToWord() throws Exception {
-		long n = 8;
-		String w = "";
+		int n = 8;
+		String w = NumberToWord.getNumberAsWords(n);
 		
 		assertEquals("eight", w);
 	}
